44f9dfdb67b18f18933ee24647161e648902ee51
NicolasLeon05 <nicolasaleon05@gmail.com>
2025-08-14 12:28:08 -0300

    Test pathfinding

---
+++ My project (2)/Assets/Scripts/EnemyPathfinding.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.SetDestination(target.position);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ My project (2)/Assets/Scripts/EnemyPathfinding2.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyPathFinding2 : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> targets = new List<GameObject>();
+    [SerializeField] private float speed;
+    private GameObject currentTarget;
+    private int currentTargetIndex;
+
+    void Start()
+    {
+        currentTargetIndex = 0;
+        currentTarget = targets[currentTargetIndex];
+    }
+
+    void Update()
+    {
+
+        if (currentTarget != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, currentTarget.transform.position, speed * Time.fixedDeltaTime);
+            CheckTargetReached();
+        }
+    }
+
+    private void CheckTargetReached()
+    {
+        float distance = Vector3.Distance(transform.position, currentTarget.transform.position);
+
+        //if (distance < math.EPSILON) 
+        //{
+        //    Debug.Log("Option 1 entered");
+        //    if(currentTargetIndex < targets.Count - 1)
+        //    {
+        //        currentTargetIndex++;
+        //        currentTarget = targets[currentTargetIndex];
+        //    }
+        //    else
+        //    {
+        //        currentTarget = null;
+        //    }
+        //}
+        if (Mathf.Approximately(distance, 0))
+        {
+            Debug.Log("Option 2 entered");
+            if (currentTargetIndex < targets.Count - 1)
+            {
+                currentTargetIndex++;
+                currentTarget = targets[currentTargetIndex];
+            }
+            else
+            {
+                currentTarget = null;
+            }
+        }
+    }
+
+
+}
